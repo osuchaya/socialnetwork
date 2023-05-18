38e9d00277ca09b5b071fd8a4efca24e75b2a95f
osuchaya <suchaya.osatis@gmail.com>
2023-05-18 15:24:55 -0700

    Created folder structures

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
