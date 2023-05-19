7a826f1b668fa1c526a16a3932288ba676c5dbdf
osuchaya <suchaya.osatis@gmail.com>
2023-05-19 15:07:41 -0700

    thoughtRoutes added

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,22 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/userController');
 
+// /api/users
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:thoughtId').delete(removeReaction);
+
+module.exports = router;
