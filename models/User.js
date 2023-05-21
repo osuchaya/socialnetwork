050a1e16396ae3ff06cce78a3d2f8d70482773a3
osuchaya <suchaya.osatis@gmail.com>
2023-05-20 22:07:35 -0700

    Added email validation for user model

--- models/User.js
+++ models/User.js
@@ -14,7 +14,13 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      
+      validate: {
+        validator: function(v) {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email"
+    },
+    required: [true, "Email required"]
     },
     thoughts: [
         {
